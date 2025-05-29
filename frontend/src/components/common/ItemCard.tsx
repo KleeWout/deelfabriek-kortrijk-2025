25dead890d3b2d0408e1af0442a97831fd74be3f
KleeWout <145778685+KleeWout@users.noreply.github.com>
2025-05-29 23:55:41 +0200

    fix: styling

--- frontend/src/components/common/ItemCard.tsx
+++ frontend/src/components/common/ItemCard.tsx
@@ -25,7 +25,7 @@ export function ItemCard({ id, title, price, imageSrc = "/assets/items/naaimachi
   };
 
   return (
-    <div className="flex flex-col w-full max-w-[340px] max-h-[280px] min-w-[140px] rounded-xl shadow-md hover:shadow-lg border-white border-2 relative cursor-pointer transition-all duration-300 hover:scale-105" onClick={() => handleItemClick(id)}>
+    <div className="flex flex-col w-full max-w-[340px] max-h-[280px] min-w-[140px] rounded-xl shadow-md hover:shadow-lg border-white border-2 relative cursor-pointer" onClick={() => handleItemClick(id)}>
       <div className={`${gradientClass} rounded-t-xl h-[116px] md:h-[200px] flex items-center justify-center p-2`}>
         {" "}
         <Image src={imageSrc} width={120} height={90} alt={title} className="object-contain max-w-full max-h-full w-[120px] h-[90px] md:w-[160px] md:h-[120px]" priority />
